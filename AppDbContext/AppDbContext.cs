6aaaed6c37baf5683ec527da2b00aae109969027
bailey <bjheinen@iastate.edu>
2022-11-22 12:25:45 -0600

    Adding DbContext and Connection String

---
+++ AppDbContext/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CySim.AppDbContext
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+            base(options)
+        {
+
+        }
+    }
+}
+
